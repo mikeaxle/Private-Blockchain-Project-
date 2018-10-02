d8eefe8f252fa2ae2ec7a2c3c08e71021552b98b
mikeaxle <mikeaxle2006@gmail.com>
2018-10-02 18:05:55 +0200

    Configure Blockchain ID Validation Routine

--- api.js
+++ api.js
@@ -8,14 +8,17 @@ const lv = require("./levelSandbox");
 
 const SHA256 = require("crypto-js/sha256");
 
+
+// memPool global array
+var memPool = [];
+
 // Create a server with a host and port
 const server = Hapi.server({
   host: "localhost",
   port: 8000
 });
 
-server.route([
-  {
+server.route([{
     // Get block route
     method: "GET",
     path: "/block/{height}",
@@ -101,6 +104,100 @@ server.route([
       } catch (error) {
         console.log(error);
       }
+    }
+  },
+  {
+    // request validation route
+    method: "POST",
+    path: "/requestValidation",
+    handler: async (request, h) => {
+      // get wallet address from request payload
+      let blockchainID = request.payload.walletAddress
+
+      // get current timestamp
+      let timestamp = Date.now()
+
+      // create response object
+      var block = {
+        address: blockchainID,
+        message: `${blockchainID}:${timestamp}:starRegistry`,
+        requestTimestamp: timestamp,
+        validationWindow: 300
+      }
+
+      // add block to memPool array
+      memPool.push(block)
+
+      // return block in response response
+      return block
+    }
+  },
+  {
+    // validate message signature
+    method: "POST",
+    path: "/message-signature/validate",
+    handler: async (request, h) => {
+      // boolean to store success 
+      let success;
+
+      // variable to store status
+      let status
+
+      // object to store response
+      let response = {}
+
+      // get address and signature from payload
+      let address = request.payload.address
+      let signature = request.payload.signature
+
+      // variable to store block from memPool
+      let block = null
+
+      // look for block matching address in memPool
+      block = memPool.find((block) => {
+        if (block.address === address) {
+          return block
+        }
+      })
+
+      // check if returned object is null
+      if (block !== undefined && block !== null) {
+        console.log(`A validation request matching the address: ${address} has been found!`)
+
+        // get time elapsed since validation request was started
+        let timeElapsed = (Date.now() - block.requestTimestamp) / 1000
+
+        // check if time elapsed is less than 5 minutes
+        if (timeElapsed <= 300) {
+
+          // set status and success
+          success = true
+          block.messageSignature = "valid"
+          block.validationWindow -= timeElapsed
+          status = block
+
+        } else {
+          // set status and success
+          success = false
+          status = `Validation window of 5 minutes has expired. Please make another validation request`
+          console.log(status)
+
+          //delete request from memPool
+          memPool.pop()
+        }
+      } else {
+        // set status and success
+        success = false
+        status = `No validation request matching the address: ${address} was found`
+        console.log(status)
+      }
+
+      // add status and success to response
+      response.registerStar = success
+      response.status = status
+
+      // return response
+      return response
     }
   }
 ]);
